b99a33e11279cbe4b682eec6e8d197d571a2f55c
AbdalrhmanEssam <103788835+AbdalrhmanEssam@users.noreply.github.com>
2025-08-18 13:02:03 +0300

    Update create-borrower.dto.ts

--- src/borrowers/dto/create-borrower.dto.ts
+++ src/borrowers/dto/create-borrower.dto.ts
@@ -3,7 +3,15 @@ import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 export class CreateBorrowerDto {
   @IsString()
   @IsNotEmpty()
-  name: string;
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
 
   @IsEmail()
   @IsNotEmpty()
